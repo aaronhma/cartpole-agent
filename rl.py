c882cc170279cd0b915e92605e21572fe8f5a39c
Aaron Ma <aaronhma@gmail.com>
2019-06-28 15:07:24 -0700

    UPDATE: rl.py

--- rl.py
+++ rl.py
@@ -128,7 +128,7 @@ def train_eval(
 
     replay_observer = [replay_buffer.add_batch]
     random_tf_policy.RandomTFPolicy()
-
+    dynamic_step_driver.DynamicStepDriver()
 def main(_):
     # Ignore all information-related logs
     logging.set_verbosity(logging.INFO)
