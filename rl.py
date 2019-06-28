dbe88eeb9dbc8ac0d4c796fa159fd041f61fce82
Aaron Ma <aaronhma@gmail.com>
2019-06-28 15:05:07 -0700

    UPDATE: rl.py

--- rl.py
+++ rl.py
@@ -33,6 +33,7 @@ flags.DEFINE_bool('use_ddqn', False,
                   'If True uses the DdqnAgent instead of the DqnAgent.')
 FLAGS = flags.FLAGS
 
+
 @gin.configurable
 def train_eval(
     root_dir,
@@ -67,21 +68,71 @@ def train_eval(
     agent_class=dqn_agent.DqnAgent,
     debug_summaries=False,
     summarize_grads_and_vars=False,
-    eval_metrics_callback=None):
-  """A simple train and eval for DQN."""
-  root_dir = os.path.expanduser(root_dir)
-  train_dir = os.path.join(root_dir, 'train')
-  eval_dir = os.path.join(root_dir, 'eval')
+        eval_metrics_callback=None):
+    """A simple train and eval for DQN."""
+    root_dir = os.path.expanduser(root_dir)
+    train_dir = os.path.join(root_dir, 'train')
+    eval_dir = os.path.join(root_dir, 'eval')
 
-  train_summary_writer = tf.compat.v2.summary.create_file_writer(
-      train_dir, flush_millis=summaries_flush_secs * 1000)
-  train_summary_writer.set_as_default()
+    train_summary_writer = tf.compat.v2.summary.create_file_writer(
+        train_dir, flush_millis=summaries_flush_secs * 1000)
+    train_summary_writer.set_as_default()
 
-  eval_summary_writer = tf.compat.v2.summary.create_file_writer(
-      eval_dir, flush_millis=summaries_flush_secs * 1000)
-  eval_metrics = [
-      py_metrics.AverageReturnMetric(buffer_size=num_eval_episodes),
-      py_metrics.AverageEpisodeLengthMetric(buffer_size=num_eval_episodes),
-  ]
+    eval_summary_writer = tf.compat.v2.summary.create_file_writer(
+        eval_dir, flush_millis=summaries_flush_secs * 1000)
+    eval_metrics = [
+        py_metrics.AverageReturnMetric(buffer_size=num_eval_episodes),
+        py_metrics.AverageEpisodeLengthMetric(buffer_size=num_eval_episodes),
+    ]
 
-  global_step = tf.compat.v1.train.get_or_create_global_step()
+    global_step = tf.compat.v1.train.get_or_create_global_step()
+    with tf.compat.v2.summary.record_if(lambda: tf.math.equal(global_step % summary_interval, 0)):
+        tf_env = tf_py_environment.TFPyEnvironment(suite_gym.load(env_name))
+        eval_py_env = suite_gym.load(env_name)
+
+        q_net = q_network.QNetwork(
+            tf_env.time_step_spec().observation,
+            tf_env.action_spec(),
+            fc_layer_params=fc_layer_params)
+
+    tf_agent = agent_class(
+        tf_env.time_step_spec(),
+        tf_env.action_spec(),
+        q_network=q_net,
+        optimizer=tf.compat.v1.train.AdamOptimizer(
+            learning_rate=learning_rate),
+        epsilon_greedy=epsilon_greedy,
+        target_update_tau=target_update_tau,
+        target_update_period=target_update_period,
+        td_errors_loss_fn=dqn_agent.element_wise_squared_loss,
+        gamma=gamma,
+        reward_scale_factor=reward_scale_factor,
+        gradient_clipping=gradient_clipping,
+        debug_summaries=debug_summaries,
+        summarize_grads_and_vars=summarize_grads_and_vars,
+        train_step_counter=global_step)
+
+    replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+        tf_agent.collect_data_spec,
+        batch_size=tf_env.batch_size,
+        max_length=replay_buffer_capacity)
+
+    eval_py_policy = py_tf_policy.PyTFPolicy(tf_agent.policy)
+
+
+def main(_):
+    # Ignore all information-related logs
+    logging.set_verbosity(logging.INFO)
+    # Enable resource variables
+    tf.enable_resource_variables()
+    agent_class = dqn_agent.DdqnAgent if FLAGS.use_ddqn else dqn_agent.DqnAgent
+    # Train the agent & evaluate it!
+    train_eval(
+        FLAGS.root_dir,
+        agent_class=agent_class,
+        num_iterations=FLAGS.num_iterations)
+
+
+if __name__ = "__main__":
+    flags.mark_flag_as_required('root_dir')
+    app.run(main)
